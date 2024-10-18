d41cb1dfbe7a9be775d52966c771af61ccd6c94e
Ben Boehmer <benboehmer@web.de>
2024-10-18 13:21:53 +0200

    Bilder updated und Readme und aufgaben.sh hinzugefügt

---
+++ aufgabe.sh
@@ -0,0 +1,43 @@
+git clone git@github.com:ben-boehmer/MeinProjekt.git
+
+cd MeinProjekt
+
+git branch -m master
+
+git config user.name "Ben Boehmer"
+
+git config user.email "benboehmer@web.de"
+
+
+#echo "print('Hello world')" > main.py
+#git add main.py
+
+#git commit -m "Initialer Commit"
+
+#git checkout -b feature
+
+#echo "print('Im feauture Zweig hinzugefuegt')" >> main.py
+#git add main.py
+
+#mkdir -p utils
+#echo "print('Ich bin ein Feature')" > utils/database.py
+#git add utils/database.py
+#git commit -m "Neue Funktion hinzugefuegt"
+
+#git checkout master
+
+#echo "print('Im Master Branch hinzugefuegt')" >> main.py
+#git add main.py
+#git commit -m "Hauptdatei aktualisiert"
+
+#git merge feature
+
+#git status
+
+#git add main.py
+
+#git commit -m "merging commit - beide änderungen übernommen"
+
+#git remote add origin https://github.com/ben-boehmer/MeinProjekt.git
+
+#git push --set-upstream origin master
